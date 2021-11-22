1bc610d08088768a4699b4db7eb9d62c679701cf
unknown <cferrara212@gmail.com>
2021-11-22 14:58:27 -0700

    instanciated inside of sellerproduct model

---
+++ eCommerceStarterCode/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using eCommerceStarterCode.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using eCommerceStarterCode.Models;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // <baseurl>/api/examples/user
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            var products = _context.Products;
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                return Ok(products);
+            }
+        }
+        
+        public IActionResult PostProduct([FromBody]Product value)
+        {
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+
+        }
+
+        public IActionResult DeleteProduct([FromBody]Product value)
+        {
+            _context.Products.Remove(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+
+        }
+    }
+}
--- eCommerceStarterCode/Models/SellerProduct.cs
+++ eCommerceStarterCode/Models/SellerProduct.cs
@@ -9,7 +9,9 @@ namespace eCommerceStarterCode.Models
     {
         [ForeignKey("SellerId")]
         public string SellerId { get; set; }
+        public Seller Seller { get; set; }
         [ForeignKey("ProductId")]
         public string ProductId { get; set; }
+        public Product Product { get; set; }
     }
 }
