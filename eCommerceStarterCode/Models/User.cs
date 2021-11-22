7d1b52af9aacb407e78519315e9af847121a6a9d
Daniel Ramirez <zoerunner@gmail.com>
2021-11-22 07:20:29 -0800

    Add shipping and billing IDs to identity user models

--- eCommerceStarterCode/Models/User.cs
+++ eCommerceStarterCode/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace eCommerceStarterCode.Models
 {
@@ -6,5 +7,13 @@ namespace eCommerceStarterCode.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [ForeignKey("ShippingAddress")]
+        public string ShippingAddressID { get; set; }  
+        public Address ShippingAddress { get; set; }
+
+        [ForeignKey("BillingAddress")]
+        public string BillingAddressID { get; set; }  
+        public Address BillingAddress { get; set; }
     }
 }
